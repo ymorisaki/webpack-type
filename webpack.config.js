d06b66a77fea6f2ce127283dfca37f8ef01f9f47
MorisakiYuji <youkourow@yahoo.co.jp>
2020-05-07 11:46:20 +0900

    コミット

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const outputPath = path.resolve(__dirname, 'docs');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = [
+  {
+    mode: 'production',
+
+    entry: {
+      index: './_dev/ts/index.ts'
+    },
+
+    devServer: {
+      contentBase: outputPath,
+      inline: true,
+      open: true,
+    },
+
+    output: {
+      filename: '[name].js',
+      path: __dirname + '/docs/shared/js'
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+        },
+      ],
+    },
+    resolve: {
+      extensions: [
+        '.ts', '.js',
+      ],
+    },
+  },
+  {
+    mode: 'production',
+    entry: {
+      style: './_dev/scss/style.scss',
+    },
+    output: {
+      filename: 'dist.txt',
+      path: __dirname + '/docs/shared/css'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            { loader: MiniCssExtractPlugin.loader },
+            { loader: 'css-loader' },
+            { loader: 'sass-loader' },
+          ],
+        }
+      ],
+    },
+    plugins:[
+      new MiniCssExtractPlugin({ filename: '[name].css' }),
+    ],
+    optimization: {
+      minimizer: [new OptimizeCSSAssetsPlugin({})],
+    },
+  }
+];
